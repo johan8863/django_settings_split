6aeb69dacae108a3859ceb7bb63169ad0e5599ea
Johan Travieso Castro <jtravieso8863@gmail.com>
2022-12-27 22:34:00 +0100

    cache for adding db_dev.sqlite3

--- django_settings_split/wsgi.py
+++ django_settings_split/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_settings_split.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_settings_split.settings.dev')
 
 application = get_wsgi_application()
